d465fc45fb72aaaadb975b81e80cf0968a70e099
Bernd Eilers <bernd.eilers@fms-logistics.com>
2019-03-14 10:13:43 +0100

    Revert "delete module-info"
    This reverts commit 0ba2b2f5611b9eeaebe16c30436e751a8fa7aaac.

---
+++ jsch-agent-proxy-core/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module net.agilhard.jsch.agentproxy.core {
+    exports net.agilhard.jsch.agentproxy.core;
+}
---
+++ jsch-agent-proxy-usocket-nc/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module net.agilhard.jsch.agentproxy.usocket.nc {
+    exports net.agilhard.jsch.agentproxy.usocket.nc;
+    requires net.agilhard.jsch.agentproxy.core;
+}
