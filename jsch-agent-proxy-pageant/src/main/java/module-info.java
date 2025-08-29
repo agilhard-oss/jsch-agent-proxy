b1cf8f562c5cd684fa6b22e8324ad0508ead66bc
bei <bernd.eilers@fms-logistics.com>
2025-08-30 01:05:45 +0200

    WIP

---
+++ jsch-agent-proxy-pageant/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module net.agilhard.jsch.agentproxy.pageant {
+    exports net.agilhard.jsch.agentproxy.connector.pageant;
+    requires net.agilhard.jsch.agentproxy.core;
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+}
